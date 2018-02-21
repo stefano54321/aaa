2089b72519320011c81dfbabdd880038fcab1b3a
Gerardo Zinno <gerardo.zinno@aol.com>
2018-02-21 16:46:42 +0100

    trovati problemi

--- gaussUML.py
+++ gaussUML.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def swap(mat, i, j):
     mat[i], mat[j] = mat[j], mat[i]
 
@@ -18,7 +20,7 @@ def eliminazione(mat):
         while  p==-1 and col<len(mat):
             col += 1
             p = find_pivot(mat, riga_pivot, col)
-            print(p)
+        #    print(p)
         if p != -1:
             swap(mat, riga_pivot, p)
             #divide
@@ -32,5 +34,11 @@ def eliminazione(mat):
 
 if __name__ == '__main__':
     a = [[0,1,3],[3,5,6],[7,8,9]]
+    b = [[10, 0, 7, 9], [12,23,45, 34], [60, 55, 7, 5], [7,3,65,7]]
+    c = [[1,2,3],[1,2,3],[4,5,6]]
     eliminazione(a)
-    print(a)
+    eliminazione(b)
+    eliminazione(c)
+    print(np.matrix(a))
+    print(np.matrix(b))
+    print(np.matrix(c))
